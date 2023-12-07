4f7ccd752902ef372c3e1c7a7ea71151e0425854
charlesoller <charlesrello@gmail.com>
2023-12-06 19:09:34 -0500

    onto phase 3

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ let server;
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
-// Your code here 
+const http = require('http');
+
+server = http.createServer((req, res) => {
+    console.log(req.method, req.url)
+})
+
+const port = 5001;
+server.listen(port, () => console.log(`Successfully started the server on port ${port}`))
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
